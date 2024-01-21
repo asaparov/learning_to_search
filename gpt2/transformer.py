Fixed some bugs with the new post-layer normalization functionality.

gpt2/transformer.py
@@ -38,7 +38,7 @@ class TransformerLayer(nn.Module):
             self.ln_ff = LayerNorm(dims)
         else:
             self.ff = None
-		self.pre_ln = pre_ln
+        self.pre_ln = pre_ln
 
     def forward(self,
                 x: torch.Tensor,
@@ -119,7 +119,7 @@ class Transformer(nn.Module):
             token_dim = words
             position_dim = 0
         self.transformers = nn.ModuleList([
-            TransformerLayer(heads, embedding_dim, token_dim, position_dim, rate, dropout, l != layers - 1, ablate, not ablate, toeplitz)
+            TransformerLayer(heads, embedding_dim, token_dim, position_dim, rate, dropout, l != layers - 1, ablate, not ablate, toeplitz, pre_ln)
             for l in range(layers)])
         self.ln_head = LayerNorm(embedding_dim)
 

train_clamp.py
@@ -18,13 +18,14 @@ class ContinuousTransformer(nn.Module):
 					rate: int = 4,
 					bidirectional: bool = True,
 					ablate: bool = True,
-					toeplitz: ToeplitzMode = ToeplitzMode.NONE):
+					toeplitz: ToeplitzMode = ToeplitzMode.NONE,
+					pre_ln: bool = True):
 		super().__init__()
 		self.bidirectional = bidirectional
 		self.future_masking = FutureMasking()
 
 		self.transformers = nn.ModuleList([
-			TransformerLayer(heads, dims, dims, 0, rate, 0, True, ablate, not ablate, toeplitz)
+			TransformerLayer(heads, dims, dims, 0, rate, 0, True, ablate, not ablate, toeplitz, pre_ln)
 			for l in range(layers)])
 
 	def to(self, device):